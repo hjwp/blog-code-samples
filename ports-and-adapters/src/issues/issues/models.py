793d115673e1504b1e7ca883690c49414c0eee96
Harry <hjwp2@cantab.net>
2018-10-14 17:05:25 +0100

    first cut of a django app that can post and view issues

--- ports-and-adapters/src/issues/issues/models.py
+++ ports-and-adapters/src/issues/issues/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class IssueReporter(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+
+
+class Issue(models.Model):
+    description = models.TextField()
+    reporter = models.ForeignKey(IssueReporter, on_delete=models.CASCADE)
+
+    def to_dict(self):
+        return {
+            'issue_id': self.id,
+            'description': self.description,
+            'reporter_name': self.reporter.name,
+            'reporter_email': self.reporter.email,
+        }
+
+    def get_absolute_url(self):
+        return reverse('issues_view', args=[self.id])
+
--- ports-and-adapters/src/issues/issues/views.py
+++ ports-and-adapters/src/issues/issues/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from issues.models import Issue, IssueReporter
 
-# Create your views here.
+@csrf_exempt
+def list_issues(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        reporter, _ = IssueReporter.objects.get_or_create(
+            name=data['reporter_name'],
+            email=data['reporter_email'],
+        )
+        issue = Issue.objects.create(
+            description=data['problem_description'],
+            reporter=reporter
+        )
+        return redirect(issue)
+
+    return HttpResponse(json.dumps([
+        issue.to_dict() for issue in Issue.objects.all()
+    ]), content_type='application/json')
+
+def view_issue(request, issue_id):
+    return HttpResponse(json.dumps(
+        Issue.objects.get(pk=issue_id).to_dict(),
+    ), content_type='application/json')
