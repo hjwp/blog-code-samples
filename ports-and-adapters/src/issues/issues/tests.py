eab17f8ca2ccf41f0605e36cd6de4411f03dfbe4
Harry <hjwp2@cantab.net>
2018-10-19 15:48:32 +0100

    start on django unit tests

--- ports-and-adapters/src/issues/issues/tests.py
+++ ports-and-adapters/src/issues/issues/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from issues.models import Issue, IssueReporter
 
 # Create your tests here.
+
+class IssueViewsTests(TestCase):
+
+    def test_view_issue(self):
+        reporter = IssueReporter.objects.create(name='jane', email='jane@example.com')
+        issue = Issue.objects.create(reporter=reporter, description='nothing works')
+        r = self.client.get(f'/issues/{issue.id}')
+        self.assertEqual(r.json(), issue.to_dict())
+
+    def test_list_issues(self):
+        reporter = IssueReporter.objects.create(name='jane', email='jane@example.com')
+        i1 = Issue.objects.create(reporter=reporter, description='nothing works')
+        i2 = Issue.objects.create(reporter=reporter, description='no blue top milk in fridge')
+        r = self.client.get('/issues/')
+        self.assertEqual(r.json(), [i1.to_dict(), i2.to_dict()])
